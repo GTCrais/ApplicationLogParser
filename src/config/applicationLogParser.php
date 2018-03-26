a91265b14420b461d5409bb5a36efecdb686f75b
GTCrais <tomislav@gtcrais.net>
2018-03-26 21:38:35 +0200

    Register Laravel's log parser for Lumen.

--- src/config/applicationLogParser.php
+++ src/config/applicationLogParser.php
@@ -5,6 +5,7 @@ return [
 	'platforms' => [
 
 		'laravel' => GTCrais\ApplicationLogParser\Parsers\Laravel\LaravelLogParser::class,
+		'lumen' => GTCrais\ApplicationLogParser\Parsers\Laravel\LaravelLogParser::class,
 
 	]
 
